835c7aa20cbdfd30c93297b12bd80cb74f1b53b9
debaranyt <daviduquemoreno@gmail.com>
2022-08-11 11:43:15 -0500

    aprendiendo rutas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (){
+    return 'Home';
 });
+
+Route::get('/usuarios', function(){
+  return 'Usuarios';
+});
+
+// Route::get('/usuarios/{id}', function($id){
+//   return "Mostrando detalle del usuario: {$id}";
+// })->where('id', '[0-9]+');
+
+Route::get('/usuarios/{id}', function($id){
+  return "Mostrando detalle del usuario: {$id}";
+})->where('id', '\d+');
+
+Route::get('/usuarios/nuevo', function(){
+  return "Crear Nuevo Usuario";
+});
+
+Route::get('/saludo/{name}/{lastname?}', function($name, $lastname = null){
+  if($lastname){
+    return "Hola {$name} {$lastname}";    
+  } else {
+    return "Hola {$name}, no tienes apodo";
+  }
+})->where('name', '\d+');
